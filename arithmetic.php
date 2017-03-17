727c9e40880d21af0603383790ee51c530790290
John Nichols <jtn0113@yahoo.com>
2017-03-17 15:59:05 -0500

    added revised arithmetic exercise -- arguments

--- arithmetic.php
+++ arithmetic.php
@@ -1,36 +1,67 @@
 <?php
 
+function throwErrorMessage() {
+	echo "ERROR: Both arguments must be numbers." . PHP_EOL; 
+}
+
 function add($a, $b)
 {
-    return $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a + $b;
+	} else {
+    	throwErrorMessage();
+    }
 }
 
 echo add(2, 5) . PHP_EOL;
 
 function subtract($a, $b)
 {
-    return $a - $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a - $b;
+    } else {
+    	throwErrorMessage();
+    }
 }
 
 echo subtract(10, 3) . PHP_EOL;
 
 function multiply($a, $b)
 {
-    return $a * $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a * $b;
+    } else {
+    	throwErrorMessage();
+    }
 }
 
-echo multiply(10, 3) . PHP_EOL;
+echo multiply(10, 2) . PHP_EOL;
 
 function divide($a, $b)
 {
-    return $a / $b;
+	if ($b != 0) {
+		if (is_numeric($a) && is_numeric($b)) {
+	    	return $a / $b;
+	    } else {
+	    	throwErrorMessage();
+	    }
+	} else {
+		return "You cannot divide by zero";
+	}
 }
 
-echo divide(100, 5) . PHP_EOL;
+echo divide(100, 10) . PHP_EOL;
 
 function modulus($a, $b)
 {
-	return $a % $b;
-}
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a % $b;
+	}  else {
+    	throwErrorMessage();
+    }
+} 
 
-echo modulus(20, 7) . PHP_EOL;
+echo modulus(20, 8) . PHP_EOL;
+
+
+// Refactor the error messages into their own throwErrorMessage() function, have the other functions use it for error messaging.
